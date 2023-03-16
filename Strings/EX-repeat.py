9261378d63a1ead00b8afbe2d7139d14564eeb54
Raíssa Azevedo <rhaii.azevedo@gmail.com>
2023-03-16 19:47:09 -0300

    Não acho o erro

--- Strings/EX-repeat.py
+++ Strings/EX-repeat.py
@@ -1,7 +1,18 @@
-def main():
-    s = open('Biblia.txt', 'r').read()
+def repetidas(lst):
+    lst_palavra = []
+    lista_contadores = [0] * len(lst)
 
-    print(s)
+    for palavra in lst:
+        if palavra not in lst_palavra:
+            lst_palavra.append(palavra)
+            lista_contadores.append(1)
+        else:
+            pos = lst_palavra.index(palavra)
+            lista_contadores += 1
 
-if __name__ == '__main__':
-    main()
+        return lst_palavra
+    
+    for i in range(len(lst_palavra)):
+        print(f"A palavra '{lst_palavra[i]}' apareceu {lista_contadores[i]} vezes na lista.")
+    
+repetidas(lst)
