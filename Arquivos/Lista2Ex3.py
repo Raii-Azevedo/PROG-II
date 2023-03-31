5a1b9d35b1c7e65bce6aff8c0f6b16a3b5716df3
Raíssa Azevedo <rhaii.azevedo@gmail.com>
2023-03-30 21:06:05 -0300

    MInha resolução

--- Arquivos/Lista2Ex3.py
+++ Arquivos/Lista2Ex3.py
@@ -2,23 +2,43 @@ def main():
     valid = []
     invalid = []
     aux = []
+    inteiros = []
     with open('EndIP.txt', 'rt') as file:
         arq = file.readline()
 
         while arq != "":
             arqsem = arq.replace('.', ' ') # Retira o "." do arquivo
-            aux.append(arqsem.strip()) # Elimina o \n da lista
+            aux.append(arqsem.strip()) # Elimina o \n da lista 
             arq = file.readline()
-        
         file.close()
-        print(aux)
 
+        for elemento in aux: # Quebrando cada elemento da lista em uma lista de strings
+            valores_str = elemento.split() 
+            valores_int = [int(valor) for valor in valores_str] # Convertendo cada valor em inteiro e adicionando à lista
+            inteiros.append(valores_int)
 
-
-
-
-
+        for lista in inteiros: # Verifica se algum valor da lista é maior que 255
+            if any(valor > 255 for valor in lista):
+                invalid.append(lista)
+                for i in range(len(invalid)):
+                    invalid[i] = str(invalid[i]).replace(',', '.')
+            else:
+                valid.append(lista)
+                for i in range(len(valid)):
+                    valid[i] = str(valid[i]).replace(',', '.')
         
+        with open('Ips.txt', 'a') as final:
+            final.write('IPS VALIDOS:\n')
+            for ip in valid:
+                final.write(ip + '\n')
+                final.write('\n')
+            final.write('IPS INVALIDOS:\n')
+            
+            for ip in invalid:
+                final.write(ip + '\n')
+                final.write('\n')
+
+        final.close()
 
 if __name__ == '__main__':
     main()
--- Arquivos/Lista2Ex3PROF.py
+++ Arquivos/Lista2Ex3PROF.py
@@ -1,58 +1,53 @@
 def valida_ip(str_ip):
-  lst = str_ip.split(".")
-  if len(lst) != 4:
-    return False
+    lst = str_ip.split(".")
+    if len(lst) != 4:
+        return False
 
-  for octeto in lst:
-    if octeto not in range(256):
-      return False
-  # for
-  return True
+    for octeto in lst:
+        if octeto not in range(256):
+            return False
+    # for
+    return True
 # fim valida_ip
 
+
 def main():
-  lista_ips = []
+    lista_ips = []
 
-  # Leitura dos ips
-  arq = open("bdips.txt","rt")
-  linha = arq.readline()
-  while linha != "":
-    lista_ips.append(linha.strip())
+    # Leitura dos ips
+    arq = open("bdips.txt", "rt")
     linha = arq.readline()
-  # while
-  arq.close()
+    while linha != "":
+        lista_ips.append(linha.strip())
+        linha = arq.readline()
+    # while
+    arq.close()
 
-  lista_val = []
-  lista_inv = []
+    lista_val = []
+    lista_inv = []
 
-  for ip in lista_ips:
-    if valida_ip(ip):
-      lista_val.append(ip)
-    else:
-      lista_inv.append(ip)
-  # for ip
+    for ip in lista_ips:
+        if valida_ip(ip):
+            lista_val.append(ip)
+        else:
+            lista_inv.append(ip)
+    # for ip
 
-  # Processando o arquivo de saída.
-  arq = open("saidabdips.txt","wt")
-  arq.write('[Endereços Válidos:]\n')
+    # Processando o arquivo de saída.
+    arq = open("saidabdips.txt", "wt")
+    arq.write('[Endereços Válidos:]\n')
 
-  for ip in lista_val:
-    arq.write(ip + '\n')
-    arq.write('\n')
 
-arq = open("saidabdips.txt","wt")
+arq = open("saidabdips.txt", "wt")
 arq.write('[Endereços Válidos:]\n')
 
-  for ip in lista_val:
-    arq.write(ip + '\n')
-    arq.write('\n')
-
-  
+   for ip in lista_val:
+        arq.write(ip + '\n')
+        arq.write('\n')
 
 
-
-
-  arq.close()
+    arq.close()
 # fim main
 
-main()
+if __name__ == '__main__':
+ main()
