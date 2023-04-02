8c16cb15ad615117fb5b53bd175c34a94be687be
Raíssa Azevedo <rhaii.azevedo@gmail.com>
2023-04-02 00:09:31 -0300

    Finalizado

--- Arquivos/Tarefa1 - Arquivos/Tarefa.py
+++ Arquivos/Tarefa1 - Arquivos/Tarefa.py
@@ -1,8 +1,8 @@
-def matriz(soma):
+def matriz(soma): # Função que recebe matriz(lista) e escreve num arquivo
     saida = input('\nDigite o nome de arquivo de saída: ')
     with open(f'{saida}.txt', 'wt') as file:
         s = ""
-        for i in range(len(soma)):
+        for i in range(len(soma)): #Laço que transforma a lista no formato de matriz
             for j in range(len(soma[0])):
                 s = s + ("%5d" % soma[i][j])
             s = s + "\n"
@@ -15,12 +15,11 @@ def somaMat(ma,mb):
     linhas = len(ma)
     colunas = len(ma[0])  
 
-    soma = [[0] * colunas for linha in range(linhas)]
+    soma = [[0] * colunas for linha in range(linhas)] # Soma recebe [0] que multiplica o número de colunas para cada linha em len de linha
 
     for i in range(linhas):
-        for j in range(colunas):
+        for j in range(colunas): # Laço para soma de duas listas em esquema de matriz
             soma[i][j] = ma[i][j] + mb[i][j]
-
     return soma
 
 
@@ -35,7 +34,7 @@ def loadmat(nomearq, arquivo):
     while linha != '':
         valores = linha.strip().split()
         if nomearq == arquivo[0]:
-            ma.append([int(valor) for valor in valores])
+            ma.append([int(valor) for valor in valores]) # Transformar em INT cada valor no laço for de valor em valores
             linha = arq.readline()
         else:
             mb.append([int(valor) for valor in valores])
