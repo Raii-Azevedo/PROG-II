aa89ba9287523d56f95cac1453151204d223231c
Raíssa Azevedo <rhaii.azevedo@gmail.com>
2023-04-18 10:52:57 -0300

    Romanos

--- Dicionário/Ex5.py
+++ Dicionário/Ex5.py
@@ -1,5 +1,6 @@
 def main():
-    d = {'I': 'um', 'II':'dois'}
+    d = {'I': 'um', 'II':'dois', 'III':'três', 'IV':'quatro', 'V':'cinco', 'VI':'seis', 'VII':'sete', 'VIII': 'oito', 'VIV': 'nove', 'X':'dez',
+         'XI':'onze'}
 
 if __name__ == '__main__':
     main()
