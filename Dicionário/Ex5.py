a63c2507df4ffec1949d9b5083d0f7f51684ae7d
Raíssa Azevedo <rhaii.azevedo@gmail.com>
2023-04-20 11:14:59 -0300

    Números Romanos

--- Dicionário/Ex5.py
+++ Dicionário/Ex5.py
@@ -1,21 +1,46 @@
-def int2romanos(dic, valor):
-    if valor in dic:
-        return dic[valor]
-    
-    else:
-        aux = ''
-        aux += dic[valor]
-        return aux 
+def soma_lst(lista):
+    soma = 0
+    for i in lista:
+        soma += i
+
+    return soma
+
+def int2romanos(dic, roman): # Função para localizar os algarismo Romanos => INT e guardar numa LST
+    lst = []
+    valint = 0
+    subvalid = [4,9,40,90,400,900] # Valores de subtração válidos em Algarismos Romanos
+
+    for valor in roman:
+        if valor not in dic:
+            return "Valor romano inválido"
+        else:
+            valint = dic[valor]
+            if lst == []:
+                lst.append(valint)
+            else:
+                if lst[-1] >= valint: # Verifica se o último número é maior ou igual ao valor informado
+                    lst.append(valint)
+                else:
+                    sub = valint - lst[-1] 
+                    if sub in subvalid: # Verifica se a subtração está dentro dos valores válidos de SUBVALID
+                        lst[-1] = sub
+                    else: # Gerou uma subtração Inválida
+                        valint = -1
+                        break
+
+    return lst
 
 
 def main():
-    d = {'': '0', '1': 'I', '2': 'II', 'C': '100', 'CC': '200', 'CCC': '300', 'CD': '400', 'CM': '900', 'D': '500', 'DC': '600', 'DCC': '700', 
-         'DCCC': '800', 'III': '3', 'IV': '4', 'IX':'9', 'L': '50', 'LX': '60', 'LXX': '70', 'LXXX': '80', 'V': '5', 'VI': '6', 'VII': '7', 'VIII': '8', 
-         'X': '10', 'XC': '90', 'XI': '11', 'XII': '12', 'XIII': '13', 'XIV': '14', 'XIX': '19', 'XL': '40', 'XV': '15', 'XVI': '16', 'XVII': '17', 
-         'XVIII': '18', 'XX':'20', 'XXX': '30'}
+    d = {"I": 1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
 
     v = input('Digite um número em Algarismos Romanos para ver em Arábico: ').upper()
-    print(int2romanos(d, v))
+    
+    lst = int2romanos(d, v)
+
+    soma = soma_lst(lst)
+
+    print(f'O algarismo romano: {v}, equivale ao número: {soma}')
 
 
 if __name__ == '__main__':
