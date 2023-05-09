7a801c550f643704d7b63cdc231f42ffab8a3999
Raíssa Azevedo <rhaii.azevedo@gmail.com>
2023-05-09 10:32:42 -0300

    Exercícios com JSON

---
+++ Dicionário/EX6.py
@@ -0,0 +1,3 @@
+print('''Construa uma programa Python que transforme a base de dados "bdcepruas.txt" em um arquivo JSON,
+A estrutura do arquivo JSON de saída seque a ordem dos campos do artigo original. Nomeie o arquivo
+de saída como "saidaexec.json" ''')
---
+++ Dicionário/EX7.py
@@ -0,0 +1,3 @@
+print(f'''Construa um programa pyhton que receba como entrada o arquivo JSON "saidaExec.json" e gere como saída
+o arquvo "exec7.json". O arquivo deve seguir a seguinte estrutura para cada dado:
+''')
