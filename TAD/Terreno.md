03dd7e5cb91d9c065c7a8dca8441bb63bd32e2db
Raissa Azevedo <rhaii.azevedo@gmail.com>
2023-06-02 08:51:12 -0300

    Terrno Py

---
+++ TAD/Terreno.md
@@ -0,0 +1,19 @@
+
+Esse código em Python define uma função chamada main() que calcula a largura e o comprimento de lotes em um terreno retangular.
+
+A biblioteca tadretangulo é importada como tret, indicando que será usada para criar objetos de lote retangular. No entanto, como não tenho informações sobre o módulo tadretangulo em minha base de conhecimento, não consigo fornecer detalhes específicos sobre seu conteúdo.
+
+Dentro da função main(), os seguintes valores são definidos:
+
+divs_l: o número de divisões (ou lotes) ao longo da largura do terreno.
+divs_c: o número de divisões (ou lotes) ao longo do comprimento do terreno.
+larg_rua: a largura das ruas entre os lotes.
+Em seguida, o programa solicita ao usuário que insira a largura e o comprimento do terreno.
+
+Os próximos cálculos são feitos para determinar a largura e o comprimento de cada lote, com base nos valores fornecidos pelo usuário e nas configurações de divisão e largura da rua.
+
+Em seguida, um loop é usado para criar objetos de lote retangular usando a função cria_lh() do módulo tadretangulo. Os objetos de lote são adicionados a uma lista chamada lotes.
+
+No código fornecido, não está claro o que é a variável sobrasTerreno mencionada nos comentários, nem está implementado o processamento ou salvamento desses dados.
+
+Por fim, o trecho if __name__ == '__main__': verifica se o código está sendo executado como um programa principal, e nesse caso, chama a função main(). Isso permite que o código seja importado como um módulo em outro programa sem que a função main() seja executada automaticamente.
--- TAD/terreno.py
+++ TAD/terreno.py
@@ -1,5 +1,33 @@
 import tadretangulo as tret
 
+def cria_lst_lotes(lt, ct, divl, divc):
+    # Calculando Largura e Comprimento de cada lote
+    # Em relação a coordenada de origem do terreno
+    larg_l = (larg_t - (divs_l +1) * larg_rua)/divs_l
+    comp_l = (comp_t - (divs_c + 1) * larg_rua)/divs_c
+
+
+    lotes = []
+    
+    # Criar uma fórmula que permita que um Loop substitua esse padrão
+    for i in range (1, divs_c + 1):
+        for j in range(divs_l):
+            lote = tret.cria_lh(j * larg_rua + (j-1) * larg_l, i * larg_rua + (i-1) * comp_t, larg_l, comp_l)
+            lotes.append(lote)
+
+    return lotes
+
+def printListaLotes(lstlotes):
+    cont = 1
+    for lote in lstlotes:
+        xy  = tret.getES(lote) # getES = Coordenada superior
+        l = tret.getL(lote)
+        c = tret.getA(lote)
+        a = tret.area(lote)
+        p = tret.perimetro(lote)
+        print("%4d%5.2f%5.2f%10.2f%10.2f%10.2f%10.2f" % (cont, xy[0], xy[1], l , c, a, p))
+        cont += 1
+
 def main():
 
     divs_l = 3
@@ -10,21 +38,9 @@ def main():
     larg_t = float(input('Entre com a largura do terreno: '))
     comp_t = float(input('Entre com o comprimento do terreno: '))
 
-    # Calculando Largura e Comprimento de cada lote
-    # Em relação a coordenada de origem do terreno
-    larg_l = (larg_t - (divs_l +1) * larg_rua)/divs_l
-    comp_l = (comp_t - (divs_c + 1) * larg_rua)/divs_c
+    # Base do loop em (sobrasTerreno) Exibir esses dados na tela e salvar no arquivo.
 
-    t1 = tret.cria_lh(1 * larg_rua + 0 * larg_l, 2, larg_l,  comp_l)
-    t2 = tret.cria_lh(2 * larg_rua + 1 * larg_l, 2, larg_l,  comp_l)
-    t3 = tret.cria_lh(3 * larg_rua + 2 * larg_l, 2, larg_l,  comp_l)
-
-    t4 = tret.cria_lh(1 * larg_rua + 0 * larg_l, 2 * larg_rua + 1 * comp_t, larg_l,  comp_l)
-    t5 = tret.cria_lh(1 * larg_rua + 0 * larg_l, 2 * larg_rua + 1 * comp_t, larg_l,  comp_l)
-    t6 = tret.cria_lh(1 * larg_rua + 0 * larg_l, 2 * larg_rua + 1 * comp_t, larg_l,  comp_l)
-
-    # Criar uma fórmula que permita que um Loop substitua esse padrão
-
+    lista_lotes = cria_lst_lotes(larg_t, comp_t, divs_l, divs_c, larg_rua)
 
 if __name__ == '__main__':
     main()
