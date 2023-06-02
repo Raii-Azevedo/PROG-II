5377868227984e61a3700ed2f3bd55d11cd80a6a
Raissa Azevedo <rhaii.azevedo@gmail.com>
2023-06-02 08:20:41 -0300

    TAD - terreno

--- TAD/terreno.py
+++ TAD/terreno.py
@@ -1,29 +1,30 @@
 import tadretangulo as tret
 
 def main():
-  divs_l = 3
-  divs_c = 4
-  larg_rua = 2
-  
-  print("EXERCÍCIO TERRENOS E LOTES V. 1.0")
-  print("=================================\n")
-  larg_t = float(input("Entre com a largura do terreno: "))
-  comp_t = float(input("Entre com o comprimento do terreno do terreno: "))
 
-  # CALCULANDO LARG E COMP DE CADA LOTE
-  larg_l = (larg_t - (divs_l + 1) * larg_rua)/divs_l
-  comp_l = (comp_t - (divs_c + 1) * larg_rua)/divs_c
+    divs_l = 3
+    divs_c = 4
+    larg_rua = 2
+    
+    print('TERRENOS E LOTES')
+    larg_t = float(input('Entre com a largura do terreno: '))
+    comp_t = float(input('Entre com o comprimento do terreno: '))
 
-  t1 = tret.cria_lh(2,2,larg_l,comp_l)
-  t2 = tret.cria_lh(?, ?,larg_l,comp_l)
-  t3 = tret.cria_lh(?, ?,larg_l,comp_l)
-  t4 = tret.cria_lh(?, ?,larg_l,comp_l)
-  t5 = tret.cria_lh(?, ?,larg_l,comp_l)
+    # Calculando Largura e Comprimento de cada lote
+    # Em relação a coordenada de origem do terreno
+    larg_l = (larg_t - (divs_l +1) * larg_rua)/divs_l
+    comp_l = (comp_t - (divs_c + 1) * larg_rua)/divs_c
 
-  
+    t1 = tret.cria_lh(1 * larg_rua + 0 * larg_l, 2, larg_l,  comp_l)
+    t2 = tret.cria_lh(2 * larg_rua + 1 * larg_l, 2, larg_l,  comp_l)
+    t3 = tret.cria_lh(3 * larg_rua + 2 * larg_l, 2, larg_l,  comp_l)
+
+    t4 = tret.cria_lh(1 * larg_rua + 0 * larg_l, 2 * larg_rua + 1 * comp_t, larg_l,  comp_l)
+    t5 = tret.cria_lh(1 * larg_rua + 0 * larg_l, 2 * larg_rua + 1 * comp_t, larg_l,  comp_l)
+    t6 = tret.cria_lh(1 * larg_rua + 0 * larg_l, 2 * larg_rua + 1 * comp_t, larg_l,  comp_l)
+
+    # Criar uma fórmula que permita que um Loop substitua esse padrão
 
 
-
-# fim main
-
-main()
+if __name__ == '__main__':
+    main()
