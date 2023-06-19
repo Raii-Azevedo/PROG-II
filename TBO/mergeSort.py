87ece9f5da0d65f40eafe6d06a696da147068533
Raissa Azevedo <rhaii.azevedo@gmail.com>
2023-06-19 15:48:36 -0300

    ok

--- TBO/mergeSort.py
+++ TBO/mergeSort.py
@@ -1,9 +1,37 @@
 def mergeSort(D):
+    if len(D) <= 1:
+        return D
+
+    meio = len(D) // 2
+    esq = D[:meio]
+    dir = D[meio:]
+
+    esq = mergeSort(esq)
+    dir = mergeSort(dir)
+
+    return merge(esq, dir)
+
+
+def merge(esq, dir):
+    merged = []
+    i = j = 0
+
+    while i < len(esq) and j < len(dir):
+        if esq[i]["marca"] < dir[j]["marca"] or esq[i]["modelo"] < dir[j]["modelo"]:
+            merged.append(esq[i])
+            i += 1
+        else:
+            merged.append(dir[j])
+            j += 1
+
+    merged.extend(esq[i:])
+    merged.extend(dir[j:])
+
+    return merged
 
 
 def lst_2_dict(lst):
     D = []
-    # Range de 0 ao Tamanha da lista de 4 em 4 items.
     for i in range(0, len(lst), 4):
         item = lst[i:i+4]
 
@@ -33,17 +61,17 @@ def txt_2_lst():
 
 def print_lst(D):
     for i in D:
-        print(i["marca"], i["modelo"], i["placa"], i["Km"],)
+        print(i["marca"], i["modelo"], i["placa"], i["Km"])
+
 
 def main():
     lst = txt_2_lst()
 
     D = lst_2_dict(lst)
-    print_lst(D)
+ 
+    mergeS = mergeSort(D)
+    print_lst(mergeS)
 
-    mergeSort(D)
-    print_lst(D)
 
-    
 if __name__ == '__main__':
     main()
